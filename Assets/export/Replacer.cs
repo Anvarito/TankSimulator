b8860506f71842635c2400b49b21f739376010f3
DESKTOP-CJBRLBE\Arvanito <anvaritohaz@gmail.com>
2023-07-03 14:47:32 +0500

    импорт Фабрики, сцена

---
+++ Assets/export/Replacer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Replacer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool _ToDynamic = true;
+    [ContextMenu("Replace")]
+    public void Replace()
+    {
+        Transform target = _ToDynamic ? GameObject.Find("_Props").transform : GameObject.Find("StaticProps").transform;
+        transform.parent = target;
+    }
+       
+
+}
